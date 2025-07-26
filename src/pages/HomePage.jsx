bf60ea0bfdefda4be8d883728139b9a5ec7ceebf
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-26 17:52:38 +0200

    feat: add RotatingSentences.jsx to HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 
 import ChooseSection from '../components/choose-section/ChooseSection.jsx'
 import FirstPage from '../components/first-page/FirstPage.jsx'
+import RotatingSentences from '../components/rotating-sentences/RotatingSentences.jsx'
 
 const HomePage = () => {
   return (
     <div>
       <FirstPage />
       <ChooseSection />
+      <RotatingSentences />
     </div>
   )
 }
