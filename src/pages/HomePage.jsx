5587b0bd282b1a2915dbf7ae62246beb24e1b77a
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-27 12:06:50 +0200

    feat: add useOutletContext

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 
+import { useOutletContext } from 'react-router-dom'
+import { useEffect } from 'react'
 import ChooseSection from '../components/choose-section/ChooseSection.jsx'
 import FirstPage from '../components/first-page/FirstPage.jsx'
 import RotatingSentences from '../components/rotating-sentences/RotatingSentences.jsx'
 
 const HomePage = () => {
+  const { setIsHomePage } = useOutletContext()
+
+  useEffect(()=>{
+    setIsHomePage(true)
+  },[]) 
+
   return (
     <div>
       <FirstPage />
