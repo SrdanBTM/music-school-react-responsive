2a10da95f6956a4d9cdddf3d185f07e7162dc1f7
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-27 11:04:25 +0200

    feat: add sentence and rotation animation

--- src/components/rotating-sentences/RotatingSentences.jsx
+++ src/components/rotating-sentences/RotatingSentences.jsx
@@ -2,9 +2,41 @@
 
 import styles from './RotatingSentences.module.css'
 import { useOutletContext } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const RotatingSentences = () => {
   const { currentLanguageData, isHomePage } = useOutletContext()
+
+  const allSentences = currentLanguageData.rotatingSentences
+
+  const [sentenceIndex, setSentenceIndex] = useState(0)
+  const [classPosition, setClassPosition] = useState('startPosition')
+
+
+  useEffect(() => {
+    setClassPosition('middlePosition')
+
+    const endPosition = setTimeout(() => {
+      setClassPosition('endPosition')
+    }, 2900)
+
+    const startPosition = setTimeout(() => {
+      setClassPosition('startPosition')
+    }, 4500)
+
+    const setSentence = setTimeout(() => {
+      setSentenceIndex(prev => prev < allSentences.length - 1 ? prev + 1 : prev = 0)
+    }, 4600)
+
+    return () => {
+      clearTimeout(endPosition)
+      clearTimeout(startPosition)
+      clearTimeout(setSentence)
+    }
+
+  }, [sentenceIndex])
+
+
 
 
   return (
@@ -15,7 +47,7 @@ const RotatingSentences = () => {
         ...(isHomePage && { marginBottom: '12rem' })
       }}>
       <div className={styles.content}>
-        
+        <p className={styles[classPosition]}>{allSentences[sentenceIndex]}</p>
       </div>
     </div>
   )
