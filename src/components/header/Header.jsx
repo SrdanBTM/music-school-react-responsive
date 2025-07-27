6849382a76f5fa6d67e5616bbb70e51935596109
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-27 12:37:01 +0200

    feat: add conditionally style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,12 @@ import styles from './Header.module.css'
 import Logo from '../logo/Logo.jsx'
 import MobileMenuButton from '../mobile-menu-button/MobileMenuButton.jsx'
 
-const Header = ({ currentLanguageData, isMobileMenuOpened, setIsMobileMenuOpened }) => {
+const Header = ({ currentLanguageData, isMobileMenuOpened, setIsMobileMenuOpened, isHomePage }) => {
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{backgroundImage: !isHomePage ? `url(${import.meta.env.BASE_URL}images/background/bg1.jpg)` : ''}}>
       <Logo
         currentLanguageData={currentLanguageData} />
 
