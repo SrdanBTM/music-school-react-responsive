18c5562eb59767b7db1598d8e5c885867b742427
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-27 17:41:54 +0200

    feat: add content in one-section container

--- src/components/one-section/OneSection.jsx
+++ src/components/one-section/OneSection.jsx
@@ -8,13 +8,38 @@ const OneSection = () => {
   const { currentLanguageData, section, setSection } = useOutletContext()
   const d = currentLanguageData
 
+
   return (
-    <Wraper title='Section'>
-      <div className={styles.container}>
-        <div>All sections</div>
-        <div>{section}</div>
-      </div>
-    </Wraper>
+    <>
+      {section && (
+        <div>
+          <Wraper title={d.sections[section].title}>
+            <div className={styles.oneSectionContainer}>
+              <h4>{d.sectionsTitles.description}</h4>
+              <p>{d.sections[section].description}</p>
+
+              <h4>{d.sectionsTitles.startDate}</h4>
+              <p>{d.sections[section].startDate}</p>
+
+              <h4>{d.sectionsTitles.instructor}</h4>
+              <p>{d.sections[section].instructorName}{d.sections[section].instructorDescription}</p>
+
+              <img
+                src={`${import.meta.env.BASE_URL}${d.sections[section].image}`}
+                alt={d.sections[section].alt} />
+                
+            </div>
+          </Wraper>
+
+          <Wraper title={d.titles.chooseYourSection}>
+            <div className={styles.allSectionsContainer}>
+              <div>All sections</div>
+            </div>
+          </Wraper>
+        </div>
+      )}
+    </>
+
   )
 }
 
