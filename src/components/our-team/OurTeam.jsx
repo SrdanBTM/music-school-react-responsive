96d76bdee4de6e8fe7970d51e4acbccc71cd9569
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-28 17:06:42 +0200

    feat: add instructors images and titles

--- src/components/our-team/OurTeam.jsx
+++ src/components/our-team/OurTeam.jsx
@@ -9,10 +9,25 @@ const OurTeam = () => {
   const { currentLanguageData } = useOutletContext()
   const d = currentLanguageData
 
+  const instructorKeys = Object.keys(d.ourTeam)
+
   return (
     <Wraper title={d.titles.ourTeam}>
       <div className={styles.container}>
-        OurTeam
+        {instructorKeys.map(key => {
+          return (
+            <div key={key} className={styles.instructor}>
+              <img
+                src={`${import.meta.env.BASE_URL}${d.ourTeam[key].img}`}
+                alt={`${d.ourTeam[key].name} photo`} />
+
+              <div className={styles.title}>
+                <h4>{d.ourTeam[key].name}</h4>
+                <h4>{d.ourTeam[key].section}</h4>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </Wraper>
   )
