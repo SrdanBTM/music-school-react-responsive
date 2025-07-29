c48cb95b040fee97bee887f1cbdc3b658d676b97
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-29 18:53:10 +0200

    feat: add conditionally paddingLeftRight0 class

--- src/components/wraper/Wraper.jsx
+++ src/components/wraper/Wraper.jsx
@@ -1,9 +1,10 @@
 
 import styles from './Wraper.module.css'
 
-const Wraper = ({ title, children }) => {
+const Wraper = ({ title, children, paddingLeftRight0 }) => {
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${paddingLeftRight0 ? styles.paddingLeftRight0 : ''}`}>
       {title && <h3>{title}</h3>}
       {children}
     </div>
