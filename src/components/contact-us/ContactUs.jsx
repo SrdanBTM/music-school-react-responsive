f3e97ae0d0c6e3ec908576e2df22044fff697b70
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-29 13:17:02 +0200

    feat: add ContactUs.jsx and ContactUs.module.css

---
+++ src/components/contact-us/ContactUs.jsx
@@ -0,0 +1,29 @@
+
+import Wraper from '../wraper/Wraper.jsx'
+import styles from './ContactUs.module.css'
+import { useOutletContext } from 'react-router-dom'
+
+const ContactUs = () => {
+
+  const { currentLanguageData } = useOutletContext()
+  const d = currentLanguageData
+
+  const contactDetailsKeys = Object.keys(d.contactDetails)
+
+  return (
+    <Wraper title={d.titles.contactDetails}>
+      <div className={styles.container}>
+        {contactDetailsKeys.map(key => {
+          return (
+            <a href={d.contactDetails[key].href} key={key}>
+              <img src={`${import.meta.env.BASE_URL}${d.contactDetails[key].image}`} alt="icon" />
+              <p>{d.contactDetails[key].title}</p>
+            </a>
+          )
+        })}
+      </div>
+    </Wraper>
+  )
+}
+
+export default ContactUs
