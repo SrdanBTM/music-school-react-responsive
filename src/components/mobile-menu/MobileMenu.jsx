f0c5d2af6a41b85eb5f29ea253747cabd6d85ed3
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-07-30 10:43:20 +0200

    refactor: simplify code

--- src/components/mobile-menu/MobileMenu.jsx
+++ src/components/mobile-menu/MobileMenu.jsx
@@ -4,15 +4,11 @@ import { NavLink } from 'react-router-dom'
 
 const MobileMenu = ({ currentLanguageData, optionalLanguageData, setLanguages, isMobileMenuOpened, setIsMobileMenuOpened }) => {
 
-  const home = currentLanguageData.nav.main.home
-  const sections = currentLanguageData.nav.main.sections
-  const aboutUs = currentLanguageData.nav.main.aboutUs
-  const contact = currentLanguageData.nav.main.contact
+  const d = currentLanguageData
 
   const en = { language1: 'en', language2: 'de' }
   const de = { language1: 'de', language2: 'en' }
 
-  
   const handleButtonClick = () => {
     setLanguages(prev => prev.language1 === 'en' ? de : en)
     setIsMobileMenuOpened(false)
@@ -22,25 +18,24 @@ const MobileMenu = ({ currentLanguageData, optionalLanguageData, setLanguages, i
     setIsMobileMenuOpened(false)
   }
 
-
   return (
     <div className={`${styles.container} ${isMobileMenuOpened ? styles.show : styles.hide}`}>
       <nav>
         <ul>
           <li>
-            <NavLink onClick={handleNavLinkClick} to='/'>{home}</NavLink>
+            <NavLink onClick={handleNavLinkClick} to='/'>{d.nav.main.home}</NavLink>
           </li>
 
           <li>
-            <NavLink onClick={handleNavLinkClick} to='/sections'>{sections}</NavLink>
+            <NavLink onClick={handleNavLinkClick} to='/sections'>{d.nav.main.sections}</NavLink>
           </li>
 
           <li>
-            <NavLink onClick={handleNavLinkClick} to='/about-us'>{aboutUs}</NavLink>
+            <NavLink onClick={handleNavLinkClick} to='/about-us'>{d.nav.main.aboutUs}</NavLink>
           </li>
 
           <li>
-            <NavLink onClick={handleNavLinkClick} to='/contact'>{contact}</NavLink>
+            <NavLink onClick={handleNavLinkClick} to='/contact'>{d.nav.main.contact}</NavLink>
           </li>
         </ul>
       </nav>
