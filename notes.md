bfb8011988138b6dd89b4bd91fea4da3519ee96d
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-09-02 14:37:15 +0200

    feat: add cover on scroll on header

---
+++ notes.md
@@ -0,0 +1,8 @@
+
+RIJESITI:
+- zatamniti header kada se scroll-uje
+- uraditi responsive
+
+
+URADIO:
+- kreirao granu cover-on-scroll
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,26 @@ import styles from './Header.module.css'
 import Logo from '../logo/Logo.jsx'
 import MobileMenuButton from '../mobile-menu-button/MobileMenuButton.jsx'
 import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react' 
 
 const Header = ({ currentLanguageData, isMobileMenuOpened, setIsMobileMenuOpened }) => {
 
+  const [ isScrolled, setIsScrolled ] = useState(false)
   const locationPathname = useLocation().pathname
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroll)
+
+    return ()=> window.removeEventListener('scroll', handleScroll)
+  },[])
+  
+  function handleScroll () {
+   (scrollY === 0 ? setIsScrolled(false) : setIsScrolled(true))
+  }
 
   return (
     <div
-      className={`${styles.container} ${locationPathname !== '/' && styles.cover}`}
+      className={`${styles.container} ${locationPathname !== '/' && styles.cover} ${isScrolled ? styles.coverBlack : styles.coverTransparent}`}
       style={{backgroundImage: locationPathname !== '/'  ? `url(${import.meta.env.BASE_URL}images/background/bg1.jpg)` : ''}}>
       <Logo
         currentLanguageData={currentLanguageData} />
