a7488cc3b98fd216ed7d4dc5cb668942c8514247
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-09-06 19:56:00 +0200

    feat: update header for small desktop

--- src/components/language-button-desktop/LanguageButtonDesktop.jsx
+++ src/components/language-button-desktop/LanguageButtonDesktop.jsx
@@ -1,22 +1,31 @@
 
 
 import styles from './LanguageButtonDesktop.module.css'
+import { useState } from 'react'
 
 const LanguageButtonDesktop = ({ currentLanguageData, optionalLanguageData, setLanguages }) => {
 
   const en = { language1: 'en', language2: 'de' }
   const de = { language1: 'de', language2: 'en' }
 
+  const [isHover, setIsHover] = useState(false)
+
   function handleButtonClick() {
     setLanguages(prev => prev.language1 === 'en' ? de : en)
   }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <button className={styles.buttonCurrent}>
         {currentLanguageData.languageButtonTitle}
       </button>
-      <button onClick={handleButtonClick} className={styles.buttonOptional}>
+      <button
+        onClick={handleButtonClick}
+        className={`${styles.buttonOptional} ${isHover ? styles.showOptionalLanguage : styles.hideOptionalLanguage}`}
+      >
         {optionalLanguageData.languageButtonTitle}
       </button>
     </div>
