d3a774bcacadc766631ccf6943c3ec81f70737b9
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-09-08 10:39:31 +0200

    feat: add small desktop responsive to OutTeam.jsx

--- src/components/our-team/OurTeam.jsx
+++ src/components/our-team/OurTeam.jsx
@@ -4,8 +4,11 @@ import Wraper from '../wraper/Wraper.jsx'
 import styles from './OurTeam.module.css'
 import { useOutletContext } from 'react-router-dom'
 import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const OurTeam = () => {
+
+  const isMobile = useMediaQuery({ maxWidth: 768 })
 
   const { currentLanguageData } = useOutletContext()
   const d = currentLanguageData
@@ -28,7 +31,7 @@ const OurTeam = () => {
 
     const clickedElementTopPositionPage = e.currentTarget.getBoundingClientRect().top + window.scrollY
     scrollTo({
-      top: clickedElementTopPositionPage - 165,
+      top: clickedElementTopPositionPage - 210,
       behavior: 'smooth'
     })
 
@@ -42,18 +45,25 @@ const OurTeam = () => {
           return (
             <div onClick={(e) => handleClick(e, key)} key={key} className={styles.instructor}>
               <div className={styles.imageAndTitle}>
-                <img className={showInfoState[key] ? styles.bigImage : styles.smallImage}
-                  src={`${import.meta.env.BASE_URL}${d.ourTeam[key].img}`}
-                  alt={`${d.ourTeam[key].name} photo`} />
+                <div className={styles.image}>
+                  <img className={showInfoState[key] ? styles.bigImage : styles.smallImage}
+                    src={`${import.meta.env.BASE_URL}${d.ourTeam[key].img}`}
+                    alt={`${d.ourTeam[key].name} photo`} />
+
+                </div>
 
                 <div className={styles.title}>
                   <h4>{d.ourTeam[key].name}</h4>
-                  <h4>{d.ourTeam[key].section}</h4>
+                  {isMobile ?
+                    <h4>{d.ourTeam[key].section}</h4>
+                    :
+                    <h4>{d.ourTeam[key].basicInfo}</h4>
+                  }
                 </div>
               </div>
 
               <div className={`${styles.info} ${showInfoState[key] ? styles.showInfo : styles.hideInfo}`}>
-                <h4>{d.ourTeam[key].basicInfo}</h4>
+                {isMobile && <h4>{d.ourTeam[key].basicInfo}</h4>}
                 <p>{d.ourTeam[key].about}</p>
               </div>
             </div>
